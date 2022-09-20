fe27e02d2b73dacf6b3a68c9846dfbd660286872
Destiny Serna <destinyeserna@gmail.com>
2022-09-19 21:19:06 -0700

    One confetti fades in and out

--- confetti.js
+++ confetti.js
@@ -1,3 +1,6 @@
+var opacity = 255;
+var speed = 4;
+frameRate = 100;
 
 function setup() {
 
@@ -9,7 +12,14 @@ function draw() {
 
     background(0);
 
-    fill(0, 255, 0);
+    fill(255, 0, 255, opacity);
 
     ellipse(windowWidth/2, windowHeight/2, 50);
+
+    if(opacity > 1){
+        opacity -= speed;
+    } else {
+        opacity = 255;
+    }
+
 }
