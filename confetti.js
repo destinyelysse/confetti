4ebf0ef56b4964b4be5d53979dee52a766689236
Destiny Serna <destinyeserna@gmail.com>
2022-09-19 21:35:58 -0700

    multiple randomly placed confetti fade in and out

--- confetti.js
+++ confetti.js
@@ -1,25 +1,34 @@
-var opacity = 255;
 var speed = 4;
 frameRate = 100;
+confetti = [];
 
 function setup() {
 
     createCanvas(windowWidth, windowHeight);
 
+    for(let i = 0; i<50; i++){
+        let confettiFlake = {
+            opacity: random(0, 255),
+            x: random(0, windowWidth),
+            y: random(0, windowHeight),
+        }
+        confetti.push(confettiFlake);
+    }
 }
 
 function draw() {
 
     background(0);
 
-    fill(255, 0, 255, opacity);
-
-    ellipse(windowWidth/2, windowHeight/2, 50);
-
-    if(opacity > 1){
-        opacity -= speed;
-    } else {
-        opacity = 255;
+    for(let i = 0; i<confetti.length;i++){
+        flake = confetti[i];
+        fill(255, 0, 255, flake.opacity);
+        ellipse(flake.x, flake.y, 50);
+        if(flake.opacity > 1){
+            flake.opacity -= speed;
+        } else {
+            flake.opacity = 255;
+        }
     }
 
 }
