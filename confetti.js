1aaa9eeb958677e7fe0d13b33945641721bb4293
Destiny Serna <destinyeserna@gmail.com>
2022-09-19 20:44:23 -0700

    initial commit

---
+++ confetti.js
@@ -0,0 +1,15 @@
+
+function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+
+}
+
+function draw() {
+
+    background(0);
+
+    fill(0, 255, 0);
+
+    ellipse(windowWidth/2, windowHeight/2, 50);
+}
